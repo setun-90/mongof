6f2d4d7c0d36c35f43a00834fd5685dae6fa7ae9
Daniel Campos do Nascimento <setun.90@gmail.com>
2019-12-19 10:33:40 +0100

    Factorisation du code

--- query.js
+++ query.js
@@ -1,19 +1,5 @@
 'use strict';
 
-// Définition de valeur floue
-function trapeze(smin, nmin, nmax, smax) {
-	this._smin = smin;
-	this._nmin = nmin;
-	this._nmax = nmax;
-	this._smax = smax;
-}
-
-function interval(min, max) {
-	this._smin = this._nmin = min;
-	this._smax = this._nmax = max;
-}
-interval.prototype = trapeze.prototype;
-
 // Fonction de traduction
 function translate(query) {
 	function traduire(field, pred) {
